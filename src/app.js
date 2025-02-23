b48959f8d60160bd779aa09b2de1fd6527835608
OGricochet <marioabel5108@gmail.com>
2025-02-23 17:32:16 +0000

    adjusted JS file to create a random excuse generator
    styling complete

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
+
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector('#excuse').innerHTML = generateExcuse();
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector('#excuse').innerHTML = generateExcuse();
+  })
 };
+
+
+let generateExcuse = () => {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  function random(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  return (
+    random(who) + " " + random(action) + " " + random(what) + " " + random(when) + "!"
+  )
+}
